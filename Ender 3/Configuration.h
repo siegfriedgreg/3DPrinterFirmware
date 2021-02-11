39657418bcae574802560659e5c53f4520ca2dc4
Gregory Siegfried <gsiegfri@eagles.nccu.edu>
2021-02-11 00:39:34 -0500

    Update Configuration.h
    fixed pin issue for neopixels

--- Ender 3/Configuration.h
+++ Ender 3/Configuration.h
@@ -701,7 +701,7 @@
 #define NEOPIXEL_LED
 #if ENABLED(NEOPIXEL_LED)
   #define NEOPIXEL_TYPE   NEO_GRB // NEO_GRBW / NEO_GRB - four/three channel driver type (defined in Adafruit_NeoPixel.h)
-  #define NEOPIXEL_PIN     24       // LED driving pin
+  #define NEOPIXEL_PIN     P1_24       // LED driving pin
   //#define NEOPIXEL2_TYPE NEOPIXEL_TYPE
   //#define NEOPIXEL2_PIN    5
   #define NEOPIXEL_PIXELS 20       // Number of LEDs in the strip. (Longest strip when NEOPIXEL2_SEPARATE is disabled.)
